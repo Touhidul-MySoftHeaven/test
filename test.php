e1ab5e1e7eb2f1e6d5aeeeeade2eb24f5c931d2b
Touhidul-Workspace <touhidul@workspaceit.com>
2024-03-09 15:13:39 +0600

    test

---
+++ test.php
@@ -0,0 +1,26 @@
+<?php
+$r=[1,2,3,4,5,6,7,8,9,10];
+
+$p=array_filter($r,function($a){
+  return $a%2==0;
+});
+print_r($p);
+
+echo "Adada";
+
+function get_odd($a)
+{
+    if($a%2 !=0) return $a;
+}
+$p=array_filter($r,"get_odd");
+  print_r($p);
+
+$k=10;
+for($i=0;$i<$k;$i++)
+{
+  //$i=3;
+  echo $i;
+  $k=3;
+}  
+echo "ada";
+echo $i;
